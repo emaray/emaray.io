456c9c001727368d6f0a950e1935cbaabcc2ae20
Blake Draper <bdraper@ugs.gov>
2017-11-12 17:18:02 -0600

    added header,pages and basic layout

---
+++ src/app/interests/interests.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-interests',
+  templateUrl: './interests.component.html',
+  styleUrls: ['./interests.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class InterestsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
